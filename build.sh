f0e5a757bdfb8ba34b4b505b61cd36f30c838d56
mindlessdrone <riaahacker456@gmail.com>
2016-05-03 20:45:16 -0400

    Simple scripts and twitter4j integrated.

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac -cp "lib/twitter4j-core-4.0.4.jar:." com/amassicci/markovbot/Main.java
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp "lib/twitter4j-core-4.0.4.jar:." com.amassicci.markovbot.Main
