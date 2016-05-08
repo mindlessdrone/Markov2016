b867c0a9aef817486fa2f78f34983071498b1d79
mindlessdrone <riaahacker456@gmail.com>
2016-05-08 09:49:14 -0400

    Modified build and run scripts to include streaming jar

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-javac -cp "lib/twitter4j-core-4.0.4.jar:." com/amassicci/markovbot/Main.java
+javac -cp "lib/twitter4j-core-4.0.4.jar:lib/twitter4j-stream-4.0.4.jar:." com/amassicci/markovbot/Tester.java
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp "lib/twitter4j-core-4.0.4.jar:." com.amassicci.markovbot.Main
+java -cp "lib/twitter4j-core-4.0.4.jar:lib/twitter4j-stream-4.0.4.jar:." com.amassicci.markovbot.Tester
