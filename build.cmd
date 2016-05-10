f2f426283d6edf2ce723db1e2775f51045561a31
mindlessdrone <riaahacker456@gmail.com>
2016-05-10 08:54:40 -0400

    Added windows scripts

---
+++ build.cmd
@@ -0,0 +1,1 @@
+javac -cp "lib\jsjf.jar:lib\twitter4j-core-4.0.4.jar:lib\twitter4j-stream-4.0.4.jar:." com\amassicci\markovbot\Tester.java
---
+++ run.cmd
@@ -0,0 +1,1 @@
+java -cp "lib\jsjf.jar:lib\twitter4j-core-4.0.4.jar:lib\twitter4j-stream-4.0.4.jar:." com.amassicci.markovbot.Tester
