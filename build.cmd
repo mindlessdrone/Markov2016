38ae85bcee9b94727999b57fbcd3769804a4996b
mindlessdrone <riaahacker456@gmail.com>
2016-05-10 09:05:52 -0400

    Some fixes to the windows scripts

--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-javac -cp "lib\jsjf.jar:lib\twitter4j-core-4.0.4.jar:lib\twitter4j-stream-4.0.4.jar:." com\amassicci\markovbot\Tester.java
+javac -cp "lib\jsjf.jar;lib\twitter4j-core-4.0.4.jar;lib\twitter4j-stream-4.0.4.jar;." com\amassicci\markovbot\Tester.java
